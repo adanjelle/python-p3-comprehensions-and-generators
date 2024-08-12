18c2382915c1cfcce4a55e90241e376a8b3eecd5
adanjelle <adanjelle4@gmail.com>
2024-08-12 11:54:52 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+
+
+# squared_minus_one = list()
+
+# for n in range(1, 11):
+#     squared_minus_one.append((n ** 2) - 1)
+
+# print(squared_minus_o
+
+
+
